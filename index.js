3cf8ae6b6c926942cfe1550fe2c5b593a8ffc71f
Michael Yin <99974993+micYn@users.noreply.github.com>
2024-03-15 17:39:37 +0800

    no

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
 function startGame(){
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
@@ -17,5 +16,5 @@ function startGame(){
         message = "You're out of the game! 😭"
         isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
 }
